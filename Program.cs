ca9f48a16712a7b02e76ae4c758dd23c39084dc7
werthwerth <teleperedozik@mail.ru>
2023-08-21 11:23:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string readText = File.ReadAllText(@"./Text1.txt");
+        var rows = readText.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        Dictionary<string, Int32> wordList = new Dictionary<string, Int32>();
+        foreach (string row in rows)
+        {
+            var words = readText.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                if (wordList.ContainsKey(word))
+                {
+                    wordList[word] = wordList[word] + 1;
+                }
+                else
+                {
+                    wordList.Add(word, 0);
+                }
+            }
+        }
+        Console.WriteLine(wordList.FirstOrDefault(x => x.Value == wordList.Values.Max()));
+    }
+}
+
